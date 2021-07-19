ec943b704ae3a3578ce098f966945ceb093bde86
Matthew Wu <mnfwu12@gmail.com>
2021-07-19 17:52:20 +0800

    update show user spaces route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
         resources :reviews, only: %i[create destroy]
         resources :bookings, only: %i[create destroy]
         get '/space/:id/review', to: 'reviews#show_space_reviews', as: 'show_space_review'
-        get '/spaces/user/:id', to: 'spaces#show_user_spaces', as: 'show_user_spaces'
       end
+      get '/spaces/user/:user_id', to: 'spaces#show_user_spaces', as: 'show_user_spaces'
       get '/bookings/user/:user_id', to: 'bookings#show_user_bookings', as: 'show_user_bookings'
     end
   end
