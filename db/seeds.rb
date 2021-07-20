f141226d525a7799fee8c9c4301637d15ad809f4
Matthew Wu <mnfwu12@gmail.com>
2021-07-20 11:08:25 +0800

    fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts '--------------'
     username: Faker::Creature::Animal.name,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    phone_number: Faker::PhoneNumber.cell_phone    open_id: rand(0..10)
+    phone_number: Faker::PhoneNumber.cell_phone
   )
   user.save!
   puts "Created user #{user.first_name}"
