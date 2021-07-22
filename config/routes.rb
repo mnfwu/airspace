00d868a2f295bf4229733a75357b28f5cde2db49
Matthew Wu <mnfwu12@gmail.com>
2021-07-22 11:50:39 +0800

    add login to wechat mp

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       get '/space/:space_id/reviews', to: 'reviews#show_space_reviews', as: 'show_space_review'
       get '/spaces/user/:user_id', to: 'spaces#show_user_spaces', as: 'show_user_spaces'
       get '/bookings/user/:user_id', to: 'bookings#show_user_bookings', as: 'show_user_bookings'
+      post '/login', to: 'login#login'
     end
   end
 end
